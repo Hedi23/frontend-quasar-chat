62bc8a83af1734f1463aa6b91a2e8864c8fe2483
Hedi Ajimi <ha@galacticweb.de>
2018-12-18 10:50:29 +0100

    Created Login Page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-  </q-page>
+    <div class="row">
+        <div class="col-12 text-center">
+            <h4>Log in or signup</h4>
+        </div>
+        <div class="col-sm-10 offset-sm-1 col-md-6 offset-md-3">
+            <div class="row">
+                <div class="col-12 q-pa-sm">
+                    <q-input
+                        v-model="login"
+                        float-label="Login"
+                        placeholder="Enter login"
+                    />
+                </div>
+                <div class="col-12 q-pa-sm">
+                    <q-input
+                        v-model="password"
+                        float-label="Password"
+                        placeholder="Enter Password"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-10 offset-sm-1 col-md-6 offset-md-3 q-pt-lg">
+            <div class="row">
+                <div class="col-6 q-pa-sm">
+                    <q-btn label="login" />
+                </div>
+                <div class="col-6 q-pa-sm">
+                    <q-btn label="signup" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style>
@@ -9,6 +39,12 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      login: '',
+      password: ''
+    }
+  }
 }
 </script>
